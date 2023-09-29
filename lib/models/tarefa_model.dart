1de36be725426a98442c5466df55a959c8be3759
MateusBolzam <mateusbolzam@gmail.com>
2023-09-29 12:28:37 -0300

    Pf Vai

--- lib/models/tarefa_model.dart
+++ lib/models/tarefa_model.dart
@@ -6,13 +6,11 @@ class Tarefa {
   int situacao; // 0 - pendente, 1 - concluído
   String detalhes;
 
-  Tarefa({
-    required this.id,
-    required this.descricao,
-    required this.data,
-    required this.hora,
-    required this.detalhes,
-    this.situacao=0
-  });
-
+  Tarefa(
+      {required this.id,
+      required this.descricao,
+      required this.data,
+      required this.hora,
+      required this.detalhes,
+      this.situacao = 0});
 }
